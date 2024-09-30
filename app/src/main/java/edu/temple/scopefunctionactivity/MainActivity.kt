f042e924cbda74c5bde4d30a243af216dd84901f
iustinherciu2000 <tuq84828@temple.edu>
2024-09-30 15:30:56 -0400

    Converted `getTestDataArray()` to a Single-Expression Function using Scope Functions.
    - set `getTestDataArray()` equals to `MutableList(10)`
    - use `Random.nextInt()` to generate a random a random integer
    - use the `apply{ sort() }` scope function to sort the list and return it.

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
 
+
     }
 
 
@@ -27,11 +28,17 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
+    private fun getTestDataArray() = MutableList(10){
+        Random.nextInt()
+    }.apply {
+        sort()
     }
+
+//    private fun getTestDataArray() : List<Int> {
+//        val testArray = MutableList(10){ Random.nextInt()}
+//        testArray.sort()
+//        return testArray
+//    }
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
